e5e468c65658bcfab0dac1875dbb7dbc7760d2d2
Alexandre <alexandr.barbier@gmail.com>
2013-01-18 11:48:19 +0100

    update Constants.cs

--- SubsonicWSTestUnit/Common/Constants.cs
+++ SubsonicWSTestUnit/Common/Constants.cs
@@ -4,11 +4,11 @@ namespace SubsonicWSTestUnit.Common
 {
     public class Constants
     {
-        public const string Login = "alex";
-        public const String Password = "narc6t5q4e";
-        public const string Server = "http://etk-a.fr";
-        public const string Port = "4040";
-        public const string Version = "1.8.0";
-        public const string UniqId = "test";
+        public const string Login = "";
+        public const String Password = "";
+        public const string Server = "";
+        public const string Port = "";
+        public const string Version = "";
+        public const string UniqId = "";
     }
 }
